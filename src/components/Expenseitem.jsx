a928dbedafffc29bf9ce4723ba5a862f4ec2add4
Akshay <121626373+Akshay-Nishal@users.noreply.github.com>
2023-06-29 17:53:31 +0530

    Passing Data

--- src/components/Expenseitem.jsx
+++ src/components/Expenseitem.jsx
@@ -1,18 +1,29 @@
 import './Expenseitem.css'
 
-export default function Expenseitem() {
+export default function Expenseitem(props) {
+  let data = props.data
+  console.log(data)
+
   const expenseDate = new Date(2023, 6, 29)
   const expenseTitle = 'Car Insurance'
   const expenseAmmount = 250
   const LocationOfExpenditure = 'Delhi'
   return (
-    <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <h3 className="expense-item__location">{LocationOfExpenditure}</h3>
-        <div className="expense-item__price">${expenseAmmount}</div>
-      </div>
+    <div>
+      {data.map((ele)=>{
+        return(
+          <div key={ele.id} className="expense-item">
+            {/* {console.log(ele.date.getDate())} */}
+            <div>{ele.date.toISOString()}</div>
+            <div className="expense-item__description">
+              <h2>{ele.title}</h2>
+              <h3 className="expense-item__location">{ele.LocationOfExpenditure}</h3>
+              <div className="expense-item__price">${ele.amount}</div>
+            </div>
+          </div>     
+        )
+      })}
     </div>
+
   )
 }
