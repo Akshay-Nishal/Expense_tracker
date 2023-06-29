08f91db01bbc2e01e94351eaa94d77ca20224394
Akshay <121626373+Akshay-Nishal@users.noreply.github.com>
2023-06-29 21:51:28 +0530

    Card fix

--- src/components/ExpenseDetails.jsx
+++ src/components/ExpenseDetails.jsx
@@ -1,6 +1,5 @@
 const ExpenseDetails = (props) => {
     let {title,location,amount} = props
-    console.log(title,location,amount)
   return (
     <div className="expense-item__description">
         <h2>{title}</h2>
--- src/components/Expenseitem.jsx
+++ src/components/Expenseitem.jsx
@@ -1,21 +1,20 @@
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
 import './Expenseitem.css'
-
+import Card from './Card'
 const Expenseitem =(props)=> {
   let data = props.data
   return (
-    <div>
+    <Card className='expenses'>
       {data.map((ele)=>{
         return(
-          <div key={ele.id} className="expense-item">
+          <Card key={ele.id} className="expense-item">
             <ExpenseDate Date={ele.date}/>
-            {console.log(ele.amount)}
             <ExpenseDetails title = {ele.title} location={ele.LocationOfExpenditure} amount = {ele.amount}/>
-          </div>     
+          </Card>     
         )
       })}
-    </div>
+    </Card>
 
   )
 }
