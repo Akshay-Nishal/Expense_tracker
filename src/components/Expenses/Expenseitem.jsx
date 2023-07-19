db498d86b6eaef1b9de99cb90d6dbd97b252599b
Akshay <121626373+Akshay-Nishal@users.noreply.github.com>
2023-07-19 19:45:59 +0530

    Manipulating the DOM - React States

--- src/components/Expenses/Expenseitem.jsx
+++ src/components/Expenses/Expenseitem.jsx
@@ -7,6 +7,7 @@ const Expenseitem =(props)=> {
   // console.log(props)
   const [data,setData] = useState(props)
   const [title,setTitle] = useState(props.title)
+  const [amount,setAmount] = useState(props.amount)
   const updateItem = (num) => {
       console.log("Updated the title")
       setTitle('Title Updated')
@@ -16,37 +17,24 @@ const Expenseitem =(props)=> {
       console.log(x)
       x.remove()
     }
+    const updateAmount = (num) => {
+      console.log("Updated the title")
+      let am = parseFloat(amount)
+      am = am+100
+      setAmount(am)
+    }
   
   return (
     // <Card className='expenses'>
       <Card id = {data.id} key={data.id} className="expense-item">
         <ExpenseDate Date={data.date}/>
-        <ExpenseDetails title = {title} location={data.LocationOfExpenditure} amount = {data.amount}/>
-        <button onClick={updateItem}>Change Title</button>
-        <button onClick={() => delItem(data.id)}>Delete Item</button>
+        <ExpenseDetails title = {title} location={data.LocationOfExpenditure} amount = {amount}/>
+        <button className='CT' onClick={updateItem}>Change Title</button>
+        <button className='Ad' onClick={updateAmount}>Add $100</button>
+        <button className='Del' onClick={() => delItem(data.id)}>Delete Item</button>
       </Card>     
     // </Card>
 
   )
 }
 export default Expenseitem
-
-    // const [data, setData] = useState(props.data)
-    // const updateItem = (num) => {
-    //   console.log(num)
-    //   let x = num
-    //   num = x.replace('e','')
-    //   num = parseInt(num)
-    //   console.log(num)
-    //   console.log("Updated the title")
-    //   let tem = data
-    //   tem[num].title = 'Updated'
-    //   console.log(tem[num])
-    //   setData(tem)
-    // }
-  
-    // function delItem(id) {
-    //   let x = document.getElementById(id)
-    //   console.log(x)
-    //   x.remove()
-    // }
