1dcb73792889ac9e79de05b5b7ef4ee827dbd1ab
Akshay <121626373+Akshay-Nishal@users.noreply.github.com>
2023-07-28 18:33:24 +0530

    Design patterns with state

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 function ExpenseForm() {
+    const [title,setTitle] = useState()
+    const [amount,setAmount] = useState()
+    const [date,setDate] = useState()
+    
+//   const [userInput,setUserInput] = useState({
+//     date:'',           //this is a method to combine multiple states together  
+//     title:'',
+//     amount:''
+//   })
     const titleChangeHandler = (event) =>{
-        console.log(event.target.value)
+        setTitle(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     title:event.target.value
+        // })
+        // setUserInput((prevState)=>{
+        //     return { ...prevState,title:event.target.value}   //this method helps to get immediate previous state and coppy it in new state, NOTEEE     prevState isnt a pre defined keyword, we can use any name we want
+        // })
+
+        // console.log(userInput.title)
+    }
+    const amountChangeHandler = (event) =>{
+        setAmount(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     amount:event.target.value
+        // })
+        // console.log(userInput.amount)
+    }
+    const dateChangeHandler = (event) =>{
+        setDate(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     date:event.target.value
+        // })
+        // console.log(userInput.date)
     }
   return (
     <form>
+        {console.log(title,amount,date)}
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label htmlFor="title">Title</label>
@@ -13,11 +48,11 @@ function ExpenseForm() {
             </div>
             <div className='new-expense__control'>
                 <label htmlFor="amount">Amount</label>
-                <input id='amount' type="number" min="0.1" step="0.1"/>
+                <input id='amount' onChange={amountChangeHandler} type="number" min="0.1" step="0.1"/>
             </div>
             <div className='new-expense__control'>
                 <label htmlFor="date">Date</label>
-                <input id='date' type="date" min="2019-01-01" max ="2022-12-31"/>
+                <input id='date' onChange={dateChangeHandler} type="date" min="2019-01-01" max ="2022-12-31"/>
             </div>
             <div className='new-expense__actions'>
                 <button type='submit'>Add Expense</button>
